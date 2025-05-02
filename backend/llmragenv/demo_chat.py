da96b963d850b261ff3d093b9389b89d0a16afab
lpz <1565561624@qq.com>
2025-05-02 10:01:23 +0800

    paper

--- backend/llmragenv/demo_chat.py
+++ backend/llmragenv/demo_chat.py
@@ -2,7 +2,7 @@
 Author: lpz 1565561624@qq.com
 Date: 2025-03-19 20:28:13
 LastEditors: lpz 1565561624@qq.com
-LastEditTime: 2025-05-01 10:32:56
+LastEditTime: 2025-05-01 23:20:23
 FilePath: /lipz/NeutronRAG/NeutronRAG/backend/llmragenv/demo_chat.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -516,7 +516,7 @@ class Demo_chat:
 
         
         print("dataset_path:",self.dataset_path)
-        data = data[offset:20]
+        data = data[offset:8]
 
         # 用于计算评估平均值
         total_queries = 0
